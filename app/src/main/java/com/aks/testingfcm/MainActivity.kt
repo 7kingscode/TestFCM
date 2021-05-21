b87ec625e68d7cb62d3647d6e866855b0a2cb81b
7KC-ANDROID <akshayraut50159832GMAIL.COM>
2021-05-21 14:53:53 +0530

    chat added

--- app/src/main/java/com/aks/testingfcm/MainActivity.kt
+++ app/src/main/java/com/aks/testingfcm/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.aks.testingfcm
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.firebase.client.Firebase
 import com.google.firebase.iid.FirebaseInstanceId
+import org.json.JSONException
+import org.json.JSONObject
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -43,10 +56,36 @@ class MainActivity : AppCompatActivity() {
             }
             builder.show()
         }
-
         var strTocken = FirebaseInstanceId.getInstance().getToken();
         Log.i("TAG", "onCreate: " + strTocken)
         print(strTocken)
+        Firebase.setAndroidContext(this);
+        var edName = findViewById<EditText>(R.id.edNAme)
+        var edPAss= findViewById<EditText>(R.id.edPass)
+        var edRegName = findViewById<EditText>(R.id.edRegNAme)
+        var edRegPas = findViewById<EditText>(R.id.edRegPass)
+
+        findViewById<Button>(R.id.btnLogin).setOnClickListener(){
+            var name=edName.text
+            var pass=edPAss.text
+            doLogin(name, pass)
+        }
+        findViewById<Button>(R.id.btnRegis).setOnClickListener(){
+            var name=edRegName.text
+            var pass=edRegPas.text
+            doRegister(name, pass)
+        }
     }
-// adding in Second Branch
+
+    private fun doLogin(name: Editable?, pass: Editable?) {
+
+
+
+    }
+
+    private fun doRegister(name: Editable?, pass: Editable?) {
+
+    }
+
+
 }
---
+++ app/src/main/java/com/aks/testingfcm/MainActivity2.kt
@@ -0,0 +1,14 @@
+package com.aks.testingfcm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+    }
+}
